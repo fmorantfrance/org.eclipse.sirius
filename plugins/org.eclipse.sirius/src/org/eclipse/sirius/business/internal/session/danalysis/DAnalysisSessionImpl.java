bc43a0686ed408b8f2db864996ef4aa67c77c529
Pierre-Charles David <pierre-charles.david@obeo.fr>
2015-01-13 16:36:35 +0100

    [DRAFT cleanup] Simplify DASI.isResourceOfSession
    Change-Id: Ic06e6d8976c00454b3f1c8876af0d0d0f7c10ac9

--- plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/DAnalysisSessionImpl.java
+++ plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/DAnalysisSessionImpl.java
@@ -1418,10 +1418,7 @@ public class DAnalysisSessionImpl extends DAnalysisSessionEObjectImpl implements
      *         {@link Session}, false otherwise
      */
     protected boolean isResourceOfSession(Resource resource, Iterable<Resource> resources) {
-        boolean isResourceOfSession = false;
-        ResourceSyncClientNotificationFilter resourceSyncClientNotificationFilter = new ResourceSyncClientNotificationFilter(resource);
-        isResourceOfSession = Iterables.any(resources, resourceSyncClientNotificationFilter);
-        return isResourceOfSession;
+        return Iterables.any(resources, new ResourceSyncClientNotificationFilter(resource));
     }
 
     /**
